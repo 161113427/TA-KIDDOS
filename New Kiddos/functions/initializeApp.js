5123c1d9d0fa45683a8739c30b0298a010fd56f1
161113427 <161113427@students.mikroskil.ac.id>
2020-08-11 17:42:49 +0700

    rollback ke Kiddos

--- New Kiddos/functions/initializeApp.js
+++ New Kiddos/functions/initializeApp.js
@@ -2,6 +2,6 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 exports.initializeApp = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://kiddos-app-c0029.firebaseio.com",
-    storageBucket: "gs://kiddos-app-c0029.appspot.com"
+    databaseURL: "https://kiddos-724ae.firebaseio.com",
+    storageBucket: "gs://kiddos-724ae.appspot.com"
 });
--- New Kiddos/public/js/firebaseConfig.js
+++ New Kiddos/public/js/firebaseConfig.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCqhIK9b4XSyBHuSVy1RxIvjPMfgdG2MCg",
-    authDomain: "kiddos-app-c0029.firebaseapp.com",
-    databaseURL: "https://kiddos-app-c0029.firebaseio.com",
-    projectId: "kiddos-app-c0029",
-    storageBucket: "kiddos-app-c0029.appspot.com",
-    messagingSenderId: "568179604167",
-    appId: "1:568179604167:web:ecad55052057fdb2dedce2",
-    measurementId: "G-FT1ZYQ65C0"
+    apiKey: "AIzaSyBBsP--COFdEvk9wc806O5-03GlXGo20wE",
+    authDomain: "kiddos-724ae.firebaseapp.com",
+    databaseURL: "https://kiddos-724ae.firebaseio.com",
+    projectId: "kiddos-724ae",
+    storageBucket: "kiddos-724ae.appspot.com",
+    messagingSenderId: "252393285870",
+    appId: "1:252393285870:web:13f8063f7d295f5c8c2bff",
+    measurementId: "G-X1913XYT0W"
 };
+
 export { firebaseConfig };
